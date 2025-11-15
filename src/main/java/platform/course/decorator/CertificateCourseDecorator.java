f14d209d6d95800279c5459ed6197fdf9394bf35
almat <almmuzdubai@gmail.com>
2025-11-16 01:01:23 +0500

    course id fixed

--- src/main/java/platform/course/decorator/CertificateCourseDecorator.java
+++ src/main/java/platform/course/decorator/CertificateCourseDecorator.java
@@ -31,9 +31,9 @@ public class CertificateCourseDecorator extends CourseDecorator {
 	@Override
 	public String toString() {
 		if (this.inner instanceof ExtraMaterialCourseDecorator) {
-			return String.format("[Course (premium) | %s] %s price, %s difficulty, %s students, %s modules, %s quizzes, features: %s", this.getTitle(), this.getPrice(), this.getDifficulty(), this.getEnrolledStudents().size(), this.getModules().size(), this.getQuizzes().size(), this.getFeatures());
+			return String.format("[Course (premium) | %s | %s id] %s price, %s difficulty, %s students, %s modules, %s quizzes, features: %s", this.getTitle(),this.getId(), this.getPrice(), this.getDifficulty(), this.getEnrolledStudents().size(), this.getModules().size(), this.getQuizzes().size(), this.getFeatures());
 		}
 
-		return String.format("[Course (w/ certificate) | %s] %s price, %s difficulty, %s students, %s modules, %s quizzes, features: %s", this.getTitle(), this.getPrice(), this.getDifficulty(), this.getEnrolledStudents().size(), this.getModules().size(), this.getQuizzes().size(), this.getFeatures());
+		return String.format("[Course (w/ certificate) | %s | %s id] %s price, %s difficulty, %s students, %s modules, %s quizzes, features: %s", this.getTitle(),this.getId(),this.getPrice(), this.getDifficulty(), this.getEnrolledStudents().size(), this.getModules().size(), this.getQuizzes().size(), this.getFeatures());
 	}
 }
